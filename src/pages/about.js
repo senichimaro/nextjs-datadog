f7bf4361c5771cd24297b26711a5900690cab31b
senichi <senichimaro@gmail.com>
2023-02-09 11:23:24 +0100

    update > exportable tracer

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,5 @@
+// DataDog Initialization
+import tracer from "../../tracer";
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
--- src/pages/requests.js
+++ src/pages/requests.js
@@ -1,8 +1,10 @@
+// DataDog Initialization
+import tracer from "../../tracer";
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
